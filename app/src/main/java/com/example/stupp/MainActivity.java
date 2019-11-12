91ba39e7a2d737d6ccc00821990df676fb398685
giabr <giabusyra@gmail.com>
2019-11-12 13:05:46 +0700

    firestore error

--- app/src/main/java/com/example/stupp/MainActivity.java
+++ app/src/main/java/com/example/stupp/MainActivity.java
@@ -13,7 +13,6 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.stupp.adapter.UserAdapter;
 import com.example.stupp.models.User;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
@@ -35,24 +34,88 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    String uid;
     FirebaseFirestore db;
     private FirebaseAuth mAuth;
+    FirestoreRecyclerAdapter<User, UserHolder> adapter;
+    RecyclerView recyclerView;
+
     String email = "joni@gmail.com";
     String password = "ayamgeprek";
-    static String order_id;
+    String uid = "FnJOziO2haVXqbYnzDeDxitYeKx2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //INITIALIZE
         FirebaseApp.initializeApp(this);
         db = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
+        recyclerView = findViewById(R.id.rv_list);
 
-        signIn(email, password);
+//        signIn(email, password);
+    }
 
+    //FIRESTORE
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //SETTING QUERY
+        FirestoreRecyclerOptions<User> options = new FirestoreRecyclerOptions.Builder<User>()
+                .setQuery(query("user","user_id",uid), User.class)
+                .build();
+
+        //ADAPTER
+        adapter = new FirestoreRecyclerAdapter<User, UserHolder>(options) {
+            @Override
+            public void onBindViewHolder(UserHolder holder, int position, User model) {
+                holder.setData(
+                        model.getUser_id()+ "\n" +
+                                model.getUsername()+ "\n" +
+                                model.getOrder_id());
+                Log.i("GET DATA", model.getUsername());
+                // Bind the Chat object to the ChatHolder
+                // ...
+            }
+
+            @Override
+            public UserHolder onCreateViewHolder(ViewGroup group, int i) {
+                // Create a new instance of the ViewHolder, in this case we are using a custom
+                // layout called R.layout.message for each item
+                View view = LayoutInflater.from(group.getContext())
+                        .inflate(R.layout.list_item, group, false);
+
+                return new UserHolder(view);
+            }
+        };
+
+        //SET ADAPTER
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(adapter != null){
+            adapter.stopListening();
+        }
+    }
+
+    //USER HOLDER
+    private class UserHolder extends RecyclerView.ViewHolder {
+        private View view;
+
+        UserHolder(View itemView) {
+            super(itemView);
+            view = itemView;
+        }
+
+        void setData(String text) {
+            TextView tv_item = view.findViewById(R.id.item);
+            tv_item.setText(text);
+        }
     }
 
     //SIGN IN
@@ -67,7 +130,6 @@ public class MainActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             FirebaseUser user = mAuth.getCurrentUser();
                             uid = user.getUid();
-                            query("user", "user_id", uid);
                             Log.i("LOGIN", uid);
                         } else {
                             Log.i("FAILED", "" + task.getException());
